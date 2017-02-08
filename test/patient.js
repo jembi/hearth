d35c3ee6285923318e54f5bc9638b2335c7659ee
hnnesv <venterh@gmail.com>
2017-02-08 15:12:54 +0200

    vread: add better check for vid format

--- test/patient.js
+++ test/patient.js
@@ -417,6 +417,48 @@ tap.test('patient should support vread', (t) => {
   })
 })
 
+// we'll never have history with non-integer values
+tap.test('should respond with 404 Not Found if vid param is not an integer', (t) => {
+  env.initDB((err, db) => {
+    t.error(err)
+
+    server.start((err) => {
+      t.error(err)
+
+      let pat = _.cloneDeep(require('./resources/Patient-1.json'))
+      delete pat.id
+
+      request.post({
+        url: 'http://localhost:3447/fhir/Patient',
+        headers: headers,
+        body: pat,
+        json: true
+      }, (err, res, body) => {
+        t.error(err)
+
+        let id = res.headers['location'].replace('/fhir/Patient/', '').replace('/_history/1', '')
+
+        request({
+          url: `http://localhost:3447/fhir/Patient/${id}/_history/bad`,
+          headers: headers,
+          json: true
+        }, (err, res) => {
+          t.error(err)
+
+          t.equal(res.statusCode, 404, 'response status code should be 404')
+
+          env.clearDB((err) => {
+            t.error(err)
+            server.stop(() => {
+              t.end()
+            })
+          })
+        })
+      })
+    })
+  })
+})
+
 tap.test('patient should support update', (t) => {
   env.initDB((err, db) => {
     t.error(err)
