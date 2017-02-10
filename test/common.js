ca7f3a93855afde040b4f1ee5a1ba013a3088a1f
hnnesv <venterh@gmail.com>
2017-02-10 11:05:36 +0200

    OHIE-198 PR #19: Add validateID

--- test/common.js
+++ test/common.js
@@ -49,3 +49,16 @@ tap.test('should resolve references', (t) => {
   t.equals(testResource.nochange.reference, 'Test/456', 'should not change non matching references')
   t.end()
 })
+
+tap.test('.util.validateID should validate FHIR id types', (t) => {
+  const common = Common(env.mongo())
+
+  t.ok(common.util.validateID('123'), '123 -> true')
+  t.ok(common.util.validateID('1.2.3'), '1.2.3 -> true')
+  t.ok(common.util.validateID('bcba5f3d-7bc5-4271-b8f8-e817b5052e23'), 'UUID -> true')
+  t.notOk(common.util.validateID('$$$'), '$$$ -> false')
+  t.ok(common.util.validateID('a'.repeat(64)), 'len(64) -> true')
+  t.ok(common.util.validateID('a'.repeat(65)), 'len(65) -> false')
+
+  t.end()
+})
