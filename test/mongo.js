33b97793c33402a1114a198c2318bc5a9dafa490
Andrew <napierandr@gmail.com>
2017-03-22 11:34:22 +0200

    HEARTH-39: Tests to ensure collapse single clauses function is working

---
+++ test/mongo.js
@@ -0,0 +1,50 @@
+'use strict'
+const mongo = require('../lib/mongo')()
+const tap = require('tap')
+
+tap.test('.util .collapseAllSingleClauses should collapse nested single clauses', (t) => {
+  let query = {
+    $and: [
+      { $or: [ { field: 'hello' } ] }
+    ]
+  }
+  t.deepEqual(mongo.util.collapseAllSingleClauses(query), { field: 'hello' })
+  t.end()
+})
+
+tap.test('.util .collapseAllSingleClauses should collapse nested single clauses', (t) => {
+  let query = {
+    $and: [
+      {
+        $or: [
+          { field1: 'hello1' },
+          { field2: 'hello2' }
+        ]
+      }
+    ]
+  }
+  t.deepEqual(mongo.util.collapseAllSingleClauses(query), { $or: [ { field1: 'hello1' }, { field2: 'hello2' } ] })
+  t.end()
+})
+
+tap.test('.util .collapseAllSingleClauses should collapse nested single clauses', (t) => {
+  let query = {
+    $and: [
+      { $or: [ { field1: 'hello1' } ] },
+      { $or: [ { field2: 'hello2' } ] }
+    ]
+  }
+  t.deepEqual(mongo.util.collapseAllSingleClauses(query), { $and: [ { field1: 'hello1' }, { field2: 'hello2' } ] } )
+  t.end()
+})
+
+tap.test('.util .collapseAllSingleClauses should collapse nested single clauses', (t) => {
+  let query = {
+    $and: [
+      { $or: [ { $and: [ { field1: 'hello1' }, { field3: 'hello3' } ] } ] },
+      { $or: [ { field2: 'hello2' } ] }
+    ]
+  }
+  t.deepEqual(mongo.util.collapseAllSingleClauses(query), { $and: [ { $or: [ { field1: 'hello1' }, { field3: 'hello3' } ] }, { field2: 'hello2' } ] } )
+  t.end()
+})
