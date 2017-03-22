c0a2b330e34a6474dee8c04573e1609261c077c3
Martin Brocker <martin@jembi.org>
2017-03-22 15:48:57 +0200

    HEARTH-55: Recursive collapse function to traverse query object

--- test/mongo.js
+++ test/mongo.js
@@ -2,17 +2,18 @@
 const mongo = require('../lib/mongo')()
 const tap = require('tap')
 
-tap.test('.util .collapseAllSingleClauses should collapse nested single clauses', (t) => {
+tap.test('.util .collapseWhenSingleClause should collapse nested single clauses', (t) => {
   let query = {
     $and: [
       { $or: [ { field: 'hello' } ] }
     ]
   }
-  t.deepEqual(mongo.util.collapseAllSingleClauses(query), { field: 'hello' })
+
+  t.deepEqual(mongo.util.collapseWhenSingleClause(query), { field: 'hello' })
   t.end()
 })
 
-tap.test('.util .collapseAllSingleClauses should collapse nested single clauses', (t) => {
+tap.test('.util .collapseWhenSingleClause should collapse nested single clauses', (t) => {
   let query = {
     $and: [
       {
@@ -23,28 +24,28 @@ tap.test('.util .collapseAllSingleClauses should collapse nested single clauses'
       }
     ]
   }
-  t.deepEqual(mongo.util.collapseAllSingleClauses(query), { $or: [ { field1: 'hello1' }, { field2: 'hello2' } ] })
+  t.deepEqual(mongo.util.collapseWhenSingleClause(query), { $or: [ { field1: 'hello1' }, { field2: 'hello2' } ] })
   t.end()
 })
 
-tap.test('.util .collapseAllSingleClauses should collapse nested single clauses', (t) => {
+tap.test('.util .collapseWhenSingleClause should collapse nested single clauses', (t) => {
   let query = {
     $and: [
       { $or: [ { field1: 'hello1' } ] },
       { $or: [ { field2: 'hello2' } ] }
     ]
   }
-  t.deepEqual(mongo.util.collapseAllSingleClauses(query), { $and: [ { field1: 'hello1' }, { field2: 'hello2' } ] } )
+  t.deepEqual(mongo.util.collapseWhenSingleClause(query), { $and: [ { field1: 'hello1' }, { field2: 'hello2' } ] } )
   t.end()
 })
 
-tap.test('.util .collapseAllSingleClauses should collapse nested single clauses', (t) => {
+tap.test('.util .collapseWhenSingleClause should collapse nested single clauses', (t) => {
   let query = {
     $and: [
       { $or: [ { $and: [ { field1: 'hello1' }, { field3: 'hello3' } ] } ] },
       { $or: [ { field2: 'hello2' } ] }
     ]
   }
-  t.deepEqual(mongo.util.collapseAllSingleClauses(query), { $and: [ { $or: [ { field1: 'hello1' }, { field3: 'hello3' } ] }, { field2: 'hello2' } ] } )
+  t.deepEqual(mongo.util.collapseWhenSingleClause(query), { $and: [ { $and: [ { field1: 'hello1' }, { field3: 'hello3' } ] }, { field2: 'hello2' } ] } )
   t.end()
 })
