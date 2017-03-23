4f1906a05e67116d10d3933ac41b408d4db20dc1
Andrew <napierandr@gmail.com>
2017-03-23 09:44:57 +0200

    HEARTH-55: Add test to ensure  operator persists

--- test/mongo.js
+++ test/mongo.js
@@ -91,3 +91,15 @@ tap.test('.util .collapseWhenSingleClause should collapse nested single clauses'
   t.deepEqual(mongo.util.collapseWhenSingleClause(query), { $and: [ { $and: [ { field1: 'hello1' }, { field3: 'hello3' } ] }, { field2: 'hello2' } ] })
   t.end()
 })
+
+tap.test('.util .collapseWhenSingleClause should collapse multiple nested clauses and a $not operator', (t) => {
+  let query = {
+    $and: [
+      { $or: [ { $and: [ { $not: { field1: 'hello1' } }, { field3: 'hello3' } ] } ] },
+      { $or: [ { field2: 'hello2' } ] },
+      { $and: [ {field4: 'hello4' } ] }
+    ]
+  }
+  t.deepEqual(mongo.util.collapseWhenSingleClause(query), { $and: [ { $and: [ { $not: { field1: 'hello1' } }, { field3: 'hello3' } ] }, { field2: 'hello2' }, { field4: 'hello4' } ] })
+  t.end()
+})
