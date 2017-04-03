42f5414cdefd7a592b784370b78d600a4f5f8cc4
Andrew <napierandr@gmail.com>
2017-04-03 15:59:39 +0200

    HEARTH-52: Add test to ensure delete works

---
+++ test/core.js
@@ -0,0 +1,105 @@
+'use strict'
+const env = require('./test-env/init')()
+const server = require('../lib/server')
+const tap = require('tap')
+const request = require('request')
+
+const headers = env.getTestAuthHeaders(env.users.sysadminUser.email)
+const testPatients = env.testPatients()
+
+const basicCoreTest = (t, test) => {
+  env.initDB((err, db) => {
+    t.error(err)
+
+    server.start((err) => {
+      t.error(err)
+      test(db, () => {
+        env.clearDB((err) => {
+          t.error(err)
+          server.stop(() => {
+            t.end()
+          })
+        })
+      })
+    })
+  })
+}
+
+const requestAndAssertResponseOperationOutcome = (tp, t, done) => {
+  // When
+  request({
+    url: `http://localhost:3447/fhir/Patient/${tp.id}`,
+    headers: headers,
+    json: true
+  }, (err, res, body) => {
+    // Then
+    t.error(err)
+    t.equal(res.statusCode, tp.statusCode, 'response status should match')
+
+    t.equal(body.resourceType, 'OperationOutcome', 'Reponse body should be an Operation Outcome')
+    t.equal(body.issue[0].severity, tp.expectedResponse.severity)
+    t.equal(body.issue[0].code, tp.expectedResponse.code)
+    t.equal(body.issue[0].details.text, tp.expectedResponse.details)
+    done()
+  })
+}
+
+tap.test('core.js', { autoend: true }, (t) => {
+  t.test('read', { autoend: true }, (t) => {
+    t.test('Read a non existent patient should return 404 - not found', (t) => {
+      basicCoreTest(t, (db, done) => {
+        const expectedResponse = {
+          severity: 'information',
+          code: 'not-found',
+          details: 'Not found'
+        }
+
+        const testParams = {
+          id: 'non-existent-id',
+          expectedResponse: expectedResponse,
+          statusCode: 404
+        }
+
+        requestAndAssertResponseOperationOutcome(testParams, t, done)
+      })
+    })
+  })
+
+  t.test('delete', { autoend: true }, (t) => {
+    t.test('Read a deleted patient should return 410 - gone', (t) => {
+      basicCoreTest(t, (db, done) => {
+        const charlton = testPatients.charlton.patient
+        charlton.id = '1111111111'
+
+        const c = db.collection('Patient')
+        c.insertOne(charlton, (err, doc) => {
+          t.error(err)
+
+          request({
+            url: `http://localhost:3447/fhir/Patient/1111111111`,
+            method: 'DELETE',
+            headers: headers,
+            json: true
+          }, (err, res, body) => {
+            t.error(err)
+            t.equal(res.statusCode, 204)
+
+            const expectedResponse = {
+              severity: 'information',
+              code: 'gone',
+              details: 'Gone'
+            }
+
+            const testParams = {
+              id: '1111111111',
+              expectedResponse: expectedResponse,
+              statusCode: 410
+            }
+
+            requestAndAssertResponseOperationOutcome(testParams, t, done)
+          })
+        })
+      })
+    })
+  })
+})
