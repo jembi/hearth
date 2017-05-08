03daecfb301c544850905ceed12d47dd6d208314
Martin Brocker <martin@jembi.org>
2017-05-08 13:35:09 +0200

    HEARTH-75: Implemeneted Matching queue patients being created/updated

---
+++ test/matching-queue-plugin.js
@@ -0,0 +1,214 @@
+'use strict'
+
+const tap = require('tap')
+const ObjectID = require('mongodb').ObjectID
+
+const env = require('./test-env/init')()
+const server = require('../lib/server')
+const request = require('request')
+
+const headers = env.getTestAuthHeaders(env.users.sysadminUser.email)
+
+const testResourceTemplate = {
+  'resourceType': 'Patient',
+  'id': '1',
+  'active': true,
+  'identifier': [
+    {
+      'use': 'official',
+      'system': 'pshr:passport:za',
+      'value': '1001113333933',
+      'assigner': {
+        'display': 'Passport South Africa'
+      }
+    }
+  ],
+  'name': [
+    {
+      'use': 'official',
+      'prefix': [
+        'Mr'
+      ],
+      'family': [
+        'Matinyana'
+      ],
+      'given': [
+        'Charlton',
+        'Joseph'
+      ]
+    }
+  ],
+  'gender': 'male',
+  'birthDate': '1970-07-21'
+}
+
+let matchingQueuePlugin
+let matchingQueuePluginTestEnv = (t, test) => {
+  env.initDB((err, db) => {
+    t.error(err)
+
+    matchingQueuePlugin = require('../lib/plugins/matching-queue')(env.mongo())
+
+    server.start((err) => {
+      t.error(err)
+
+      test(db, () => {
+        env.clearDB((err) => {
+          t.error(err)
+          server.stop(() => {
+            t.end()
+          })
+        })
+      })
+    })
+  })
+}
+
+tap.test('Matching Queue Plugin - should NOT add the patient resource to the matching queue collection - interaction not supported (read)', (t) => {
+  // given
+  matchingQueuePluginTestEnv(t, (db, done) => {
+    const resource = JSON.parse(JSON.stringify(testResourceTemplate))
+
+    // when
+    matchingQueuePlugin.hooks.after[0].function('read', {}, 'Patient', resource, (err, badRequest) => {
+      // then
+      t.error(err)
+      t.ok(badRequest)
+      t.equal(badRequest.message, 'Supplied interaction "read" is not supported in this plugin', `should return a bad request error: Supplied interaction "read" is not supported in this plugin`)
+
+      done()
+    })
+  })
+})
+
+tap.test('Matching Queue Plugin - should add the patient resource to the matching queue collection', (t) => {
+  // given
+  matchingQueuePluginTestEnv(t, (db, done) => {
+    const resource = JSON.parse(JSON.stringify(testResourceTemplate))
+
+    // when
+    matchingQueuePlugin.hooks.after[0].function('create', {}, 'Patient', resource, (err, badRequest, queueObj) => {
+      // then
+      t.error(err)
+      t.error(badRequest)
+      t.ok(queueObj)
+
+      let c = db.collection('matchingQueue')
+      c.findOne({ _id: ObjectID(queueObj.queueId) }, (err, doc) => {
+        t.error(err)
+
+        t.equal(doc._id.toString(), queueObj.queueId, `should return a queued document with id: ${queueObj.queueId}`)
+        t.equal(doc.payload.active, true, `should return a queued document with a status of: active`)
+        t.equal(doc.payload.identifier[0].system, 'pshr:passport:za', `should return a queued document with a identifier system of: pshr:passport:za`)
+        t.equal(doc.payload.identifier[0].value, '1001113333933', `should return a queued document with a identifier value of: 1001113333933`)
+        t.equal(doc.payload.name[0].prefix[0], 'Mr', `should return a queued document with a name prefix of: Mr`)
+        t.equal(doc.payload.name[0].given[0], 'Charlton', `should return a queued document with a given name of: Charlton`)
+        t.equal(doc.payload.name[0].given[1], 'Joseph', `should return a queued document with a given name of: Joseph`)
+        t.equal(doc.payload.name[0].family[0], 'Matinyana', `should return a queued document with a family name of: Matinyana`)
+        t.equal(doc.payload.gender, 'male', `should return a queued document with a gender of: male`)
+        t.equal(doc.payload.birthDate, '1970-07-21', `should return a queued document with a birthDate of: 1970-07-21`)
+
+        done()
+      })
+    })
+  })
+})
+
+tap.test('Matching Queue Plugin - should add the patient resource to the matching queue collection via create API request', (t) => {
+  // given
+  matchingQueuePluginTestEnv(t, (db, done) => {
+    const resource = JSON.parse(JSON.stringify(testResourceTemplate))
+    delete resource.id
+
+    // when
+    request.post({
+      url: 'http://localhost:3447/fhir/Patient',
+      headers: headers,
+      body: resource,
+      json: true
+    }, (err, res, body) => {
+      // then
+      t.error(err)
+      t.ok(body)
+
+      const patientId = res.headers.location.replace('/fhir/Patient/', '').replace(/\/_history\/.*/, '')
+
+      let c = db.collection('matchingQueue')
+      c.findOne({ 'payload.id': patientId }, (err, doc) => {
+        t.error(err)
+
+        t.equal(doc.payload.active, true, `should return a queued document with a status of: active`)
+        t.equal(doc.payload.identifier[0].system, 'pshr:passport:za', `should return a queued document with a identifier system of: pshr:passport:za`)
+        t.equal(doc.payload.identifier[0].value, '1001113333933', `should return a queued document with a identifier value of: 1001113333933`)
+
+        done()
+      })
+    })
+  })
+})
+
+tap.test('Matching Queue Plugin - should add the patient resource to the matching queue collection via update API request', (t) => {
+  // given
+  matchingQueuePluginTestEnv(t, (db, done) => {
+    const resource = JSON.parse(JSON.stringify(testResourceTemplate))
+    delete resource.id
+
+    // save
+    request.post({
+      url: 'http://localhost:3447/fhir/Patient',
+      headers: headers,
+      body: resource,
+      json: true
+    }, (err, res, body) => {
+      t.error(err)
+
+      const id = res.headers.location.replace('/fhir/Patient/', '').replace(/\/_history\/.*/, '')
+      const update = {
+        resourceType: 'Patient',
+        id: id,
+        active: true,
+        name: [
+          {
+            given: ['Update']
+          }
+        ]
+      }
+
+      // update
+      request.put({
+        url: `http://localhost:3447/fhir/Patient/${id}`,
+        headers: headers,
+        body: update,
+        json: true
+      }, (err, res) => {
+        t.error(err)
+
+        t.equal(res.statusCode, 200, 'response status code should be 200')
+
+        request({
+          url: `http://localhost:3447/fhir/Patient/${id}`,
+          headers: headers,
+          json: true
+        }, (err, res, body) => {
+          t.error(err)
+
+          t.equal(res.statusCode, 200, 'response status code should be 200')
+          t.ok(body)
+          t.equal(body.resourceType, 'Patient', 'result should be a patient')
+          t.equal(body.name[0].given[0], 'Update', 'body should contain the latest patient')
+
+          let c = db.collection('matchingQueue')      
+          c.findOne({ 'payload.id': id }, (err, doc) => {
+            t.error(err)
+
+            t.equal(doc.payload.active, true, `should return a queued document with a status of: active`)
+            t.equal(doc.payload.identifier[0].system, 'pshr:passport:za', `should return a queued document with a identifier system of: pshr:passport:za`)
+            t.equal(doc.payload.identifier[0].value, '1001113333933', `should return a queued document with a identifier value of: 1001113333933`)
+
+            done()
+          })
+        })
+      })
+    })
+  })
+})
