c72d5d759bf6a381eed739a64b20bac761a16aa5
Andrew <napierandr@gmail.com>
2017-05-15 14:36:10 +0200

    HEARTH-87: Config option to enable indexing on matching fields

---
+++ test/matching-indexes.js
@@ -0,0 +1,55 @@
+'use strict'
+
+process.env.NODE_ENV = 'test'
+require('../lib/init')
+
+const tap = require('tap')
+const async = require('neo-async')
+
+const mongo = require('../lib/mongo')()
+const matchingIndexes = require('../scripts/matching-indexes')(mongo)
+const resourceConfig = require('../config/matching').resourceConfig
+
+const matchingIndexesTest = (t, test) => {
+  mongo.getDB((err, db) => {
+    t.error(err)
+
+    matchingIndexes.createMatchingIndexes((err) => {
+      t.error(err)
+
+      test(db, () => {
+        db.dropDatabase((err) => {
+          t.error(err)
+
+          mongo.closeDB(() => {
+            t.error(err)
+            t.end()
+          })
+        })
+      })
+    })
+  })
+}
+
+tap.test('should ensure indexes are created', (t) => {
+  matchingIndexesTest(t, (db, done) => {
+    const tasks = []
+    Object.keys(resourceConfig).forEach((collection) => {
+      const c = db.collection(collection)
+
+      Object.keys(resourceConfig[collection].matchingProperties).forEach((path) => {
+        tasks.push((callback) => c.indexExists(`${path}_1`, callback))
+        tasks.push((callback) => c.indexExists(`_transforms.matching.${path}_1`, callback))
+      })
+    })
+
+    async.parallel(tasks, (err, results) => {
+      t.error(err)
+
+      results.forEach((result) => {
+        t.true(result)
+      })
+      done()
+    })
+  })
+})
