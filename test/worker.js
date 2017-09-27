26bfad4b3ea41bb1a9cf34cb36c433acf8c31a3a
Martin Brocker <martin@jembi.org>
2017-09-27 15:37:24 +0200

    Test fix attempt for Travis

--- test/worker.js
+++ test/worker.js
@@ -67,9 +67,11 @@ const matchingQueueTest = (queueSize, t, test) => {
       test(db, (testWorkers) => {
         env.clearDB((err) => {
           t.error(err)
+
           Object.keys(testWorkers).forEach((key) => {
             testWorkers[key].kill('SIGINT')
           })
+          
           t.end()
         })
       })
@@ -78,6 +80,92 @@ const matchingQueueTest = (queueSize, t, test) => {
     })
   })
 }
+
+tap.test('should successfully update score of patient links', (t) => {
+  const testPatient1 = JSON.parse(JSON.stringify(patientResource))
+  testPatient1.id = '1111111111'
+  testPatient1._transforms.matching.name[0].given[0] = doubleMetaphone(testPatient1.name[0].given[0])
+  testPatient1._transforms.matching.name[0].family[0] = doubleMetaphone(testPatient1.name[0].family[0])
+  testPatient1.link = [
+    {
+      other: {
+        reference: 'Patient/2222222222'
+      },
+      type: 'possible-duplicate-source',
+      extension: [{
+        url: 'http://hearth.org/link-matching-score',
+        valueDecimal: 0.67
+      }]
+    }
+  ]
+
+  const testPatient2 = JSON.parse(JSON.stringify(patientResource))
+  testPatient2.id = '2222222222'
+  testPatient2._transforms.matching.name[0].given[0] = doubleMetaphone(testPatient2.name[0].given[0])
+  testPatient2._transforms.matching.name[0].family[0] = doubleMetaphone(testPatient2.name[0].family[0])
+  testPatient2.link = [
+    {
+      other: {
+        reference: 'Patient/11111111111'
+      },
+      type: 'possible-duplicate-of',
+      extension: [{
+        url: 'http://hearth.org/link-matching-score',
+        valueDecimal: 0.77
+      }]
+    }
+  ]
+
+  env.initDB((err, db) => {
+    t.error(err)
+
+    const c = db.collection('Patient')
+    c.insertMany([testPatient1, testPatient2], (err) => {
+      t.error(err)
+
+      c.find().toArray((err, result) => {
+        t.error(err)
+
+        t.equal(result[0].id, '1111111111', 'Patient1 successfully created')
+        t.equal(result[0].link[0].extension[0].valueDecimal, 0.67)
+        t.equal(result[1].id, '2222222222', 'Patient2 successfully created')
+        t.equal(result[1].link[0].extension[0].valueDecimal, 0.77)
+        const matchingQueue = mongoDbQueue(db, constants.MATCHING_QUEUE_COLLECTION)
+        matchingQueue.add(testPatient2, (err) => {
+          t.error(err)
+
+          const testWorker = cp.fork(`${__dirname}/../lib/matching-queue/worker.js`, ['testWorker'])
+          const messages = []
+          testWorker.on('message', (msg) => {
+            t.error(msg.error)
+
+            messages.push(msg)
+            if (messages.length === 3) {
+              t.equal(messages[0], 'testWorker started')
+              t.equal(messages[1].info.substring(0, messages[1].info.length - 24), 'testWorker Successfully processed queue element with id: ')
+              t.equal(messages[2].debug, 'testWorker No records in queue')
+
+              c.find().toArray((err, results) => {
+                t.error(err)
+
+                t.equal(results.length, 2, 'should be two patients')
+                t.equal(results[0].id, '1111111111')
+                t.equal(results[0].link[0].extension[0].valueDecimal, 1)
+                t.equal(results[1].id, '2222222222')
+                t.equal(results[1].link[0].extension[0].valueDecimal, 1)
+                env.clearDB((err) => {
+                  t.error(err)
+                  testWorker.kill()
+                  t.end()
+                })
+              })
+            }
+          })
+        })
+      })
+    })
+  })
+})
 
 tap.test('should create a size one queue and start one worker to read off the queue', (t) => {
   config.setConf('matchingQueue:numberOfWorkers', 1)
@@ -172,92 +260,6 @@ tap.test('should create a size 10 queue and start 5 workers to read off the queu
       })
     }).catch((err) => {
       t.error(err)
-    })
-  })
-})
-
-tap.test('should successfully update score of patient links', (t) => {
-  const testPatient1 = JSON.parse(JSON.stringify(patientResource))
-  testPatient1.id = '1111111111'
-  testPatient1._transforms.matching.name[0].given[0] = doubleMetaphone(testPatient1.name[0].given[0])
-  testPatient1._transforms.matching.name[0].family[0] = doubleMetaphone(testPatient1.name[0].family[0])
-  testPatient1.link = [
-    {
-      other: {
-        reference: 'Patient/2222222222'
-      },
-      type: 'possible-duplicate-source',
-      extension: [{
-        url: 'http://hearth.org/link-matching-score',
-        valueDecimal: 0.67
-      }]
-    }
-  ]
-
-  const testPatient2 = JSON.parse(JSON.stringify(patientResource))
-  testPatient2.id = '2222222222'
-  testPatient2._transforms.matching.name[0].given[0] = doubleMetaphone(testPatient2.name[0].given[0])
-  testPatient2._transforms.matching.name[0].family[0] = doubleMetaphone(testPatient2.name[0].family[0])
-  testPatient2.link = [
-    {
-      other: {
-        reference: 'Patient/11111111111'
-      },
-      type: 'possible-duplicate-of',
-      extension: [{
-        url: 'http://hearth.org/link-matching-score',
-        valueDecimal: 0.77
-      }]
-    }
-  ]
-
-  env.initDB((err, db) => {
-    t.error(err)
-
-    const c = db.collection('Patient')
-    c.insertMany([testPatient1, testPatient2], (err) => {
-      t.error(err)
-
-      c.find().toArray((err, result) => {
-        t.error(err)
-
-        t.equal(result[0].id, '1111111111', 'Patient1 successfully created')
-        t.equal(result[0].link[0].extension[0].valueDecimal, 0.67)
-        t.equal(result[1].id, '2222222222', 'Patient2 successfully created')
-        t.equal(result[1].link[0].extension[0].valueDecimal, 0.77)
-        const matchingQueue = mongoDbQueue(db, constants.MATCHING_QUEUE_COLLECTION)
-        matchingQueue.add(testPatient2, (err) => {
-          t.error(err)
-
-          const testWorker = cp.fork(`${__dirname}/../lib/matching-queue/worker.js`, ['testWorker'])
-          const messages = []
-          testWorker.on('message', (msg) => {
-            t.error(msg.error)
-
-            messages.push(msg)
-            if (messages.length === 3) {
-              t.equal(messages[0], 'testWorker started')
-              t.equal(messages[1].info.substring(0, messages[1].info.length - 24), 'testWorker Successfully processed queue element with id: ')
-              t.equal(messages[2].debug, 'testWorker No records in queue')
-
-              c.find().toArray((err, results) => {
-                t.error(err)
-
-                t.equal(results.length, 2, 'should be two patients')
-                t.equal(results[0].id, '1111111111')
-                t.equal(results[0].link[0].extension[0].valueDecimal, 1)
-                t.equal(results[1].id, '2222222222')
-                t.equal(results[1].link[0].extension[0].valueDecimal, 1)
-                env.clearDB((err) => {
-                  t.error(err)
-                  testWorker.kill()
-                  t.end()
-                })
-              })
-            }
-          })
-        })
-      })
     })
   })
 })
