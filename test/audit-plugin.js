8d1ede17f4ed04a17398f73ecc8ea87b29e74042
Martin Brocker <martin@jembi.org>
2017-10-31 08:24:40 +0200

    ANSA-171: add fhirResource object to plugins, updated tests

--- test/audit-plugin.js
+++ test/audit-plugin.js
@@ -13,11 +13,16 @@ const moment = require('moment')
 
 const env = require('./test-env/init')()
 const server = require('../lib/server')
-const auditPlugin = require('../lib/plugins/audit')()
+
+let auditPlugin
+
+const fhirResources = { AuditEvent: { name: 'AuditEvent', searchFilters: () => {} } }
 
 const testServerInit = (t, test) => {
   env.initDB((err, db) => {
     t.error(err)
+
+    auditPlugin = require('../lib/plugins/audit')(env.mongo(), fhirResources)
 
     server.start((err) => {
       t.error(err)
@@ -69,171 +74,185 @@ const testResourceTemplate = {
 
 tap.test('Audit Plugin - getSuccessOrFailed()', { autoend: true }, (t) => {
   t.test('should determine the event outcome value for a successfull request', (t) => {
-    // given
-    const resource = JSON.parse(JSON.stringify(testResourceTemplate))
+    testServerInit(t, (db, done) => {
+      // given
+      const resource = JSON.parse(JSON.stringify(testResourceTemplate))
 
-    // when
-    const successOrFailValue = auditPlugin.getSuccessOrFailed(resource)
+      // when
+      const successOrFailValue = auditPlugin.getSuccessOrFailed(resource)
 
-    t.equal(successOrFailValue, 0, `should return a success/fail value of 0 - Success`)
+      t.equal(successOrFailValue, 0, `should return a success/fail value of 0 - Success`)
 
-    t.end()
+      done()
+    })
   })
 
   t.test('should determine the event outcome value for a unsuccessfull request', (t) => {
-    // given
-    const resource = {
-      resourceType: 'OperationOutcome',
-      issue: [{ 'severity': 'information', 'code': 'gone', 'details': { 'text': 'Gone' } }]
-    }
+    testServerInit(t, (db, done) => {
+      // given
+      const resource = {
+        resourceType: 'OperationOutcome',
+        issue: [{ 'severity': 'information', 'code': 'gone', 'details': { 'text': 'Gone' } }]
+      }
 
-    // when
-    const successOrFailValue = auditPlugin.getSuccessOrFailed(resource)
+      // when
+      const successOrFailValue = auditPlugin.getSuccessOrFailed(resource)
 
-    t.equal(successOrFailValue, 4, `should return a success/fail value of 4 - Minor Failure`)
+      t.equal(successOrFailValue, 4, `should return a success/fail value of 4 - Minor Failure`)
 
-    t.end()
+      done()
+    })
   })
 })
 
 tap.test('Audit Plugin - buildEventObj()', { autoend: true }, (t) => {
   t.test('should build the eventObj object', (t) => {
-    // given
-    const resource = JSON.parse(JSON.stringify(testResourceTemplate))
+    testServerInit(t, (db, done) => {
+      // given
+      const resource = JSON.parse(JSON.stringify(testResourceTemplate))
 
-    // when
-    const eventObj = auditPlugin.buildEventObj('read', resource)
+      // when
+      const eventObj = auditPlugin.buildEventObj('read', resource)
 
-    t.ok(eventObj)
+      t.ok(eventObj)
 
-    t.equals(eventObj.type.coding[0].code, 'rest', 'should have a value of \'rest\'')
-    t.equals(eventObj.type.coding[0].display, 'Restful Operation', 'should have a value of \'Restful Operation\'')
-    t.equals(eventObj.subtype[0].coding[0].code, 'read', 'should have a value of \'read\'')
-    t.equals(eventObj.subtype[0].coding[0].display, 'read', 'should have a value of \'read\'')
-    t.equals(moment(eventObj.dateTime).isValid(), true, 'should be a valid timestamp value')
-    t.equals(eventObj.outcome, 0, 'should have a value of \'0\'')
+      t.equals(eventObj.type.coding[0].code, 'rest', 'should have a value of \'rest\'')
+      t.equals(eventObj.type.coding[0].display, 'Restful Operation', 'should have a value of \'Restful Operation\'')
+      t.equals(eventObj.subtype[0].coding[0].code, 'read', 'should have a value of \'read\'')
+      t.equals(eventObj.subtype[0].coding[0].display, 'read', 'should have a value of \'read\'')
+      t.equals(moment(eventObj.dateTime).isValid(), true, 'should be a valid timestamp value')
+      t.equals(eventObj.outcome, 0, 'should have a value of \'0\'')
 
-    t.end()
+      done()
+    })
   })
 })
 
 tap.test('Audit Plugin - buildParticipantObj()', { autoend: true }, (t) => {
   t.test('should build the participantObj object', (t) => {
-    // given
-    const ctx = {
-      authenticatedUser: {
-        email: 'user@hearth.org',
-        type: 'practitioner',
-        resource: 'Practitioner/b7aeb450-8bde-11e7-812f-bbfc0872406b'
-      },
-      url: '/fhir/Questionnaire?identifier=preoperative-questionnaire',
-      query: { identifier: 'preoperative-questionnaire' },
-      headers: {
-        referer: 'http://localhost:9000/'
+    testServerInit(t, (db, done) => {
+      // given
+      const ctx = {
+        authenticatedUser: {
+          email: 'user@hearth.org',
+          type: 'practitioner',
+          resource: 'Practitioner/b7aeb450-8bde-11e7-812f-bbfc0872406b'
+        },
+        url: '/fhir/Questionnaire?identifier=preoperative-questionnaire',
+        query: { identifier: 'preoperative-questionnaire' },
+        headers: {
+          referer: 'http://localhost:9000/'
+        }
       }
-    }
 
-    // when
-    const participantObj = auditPlugin.buildParticipantObj(ctx)
+      // when
+      const participantObj = auditPlugin.buildParticipantObj(ctx)
 
-    t.ok(participantObj)
+      t.ok(participantObj)
 
-    t.equals(participantObj.role, 'practitioner', 'should have a value of \'practitioner\'')
-    t.equals(participantObj.reference, 'Practitioner/b7aeb450-8bde-11e7-812f-bbfc0872406b', 'should have a value of \'Practitioner/b7aeb450-8bde-11e7-812f-bbfc0872406b\'')
-    t.equals(participantObj.userId, 'b7aeb450-8bde-11e7-812f-bbfc0872406b', 'should have a value of \'b7aeb450-8bde-11e7-812f-bbfc0872406b\'')
-    t.equals(participantObj.altId, 'user@hearth.org', 'should have a value of \'user@hearth.org\'')
-    t.equals(participantObj.requester, true, 'should have a value of \'true\'')
+      t.equals(participantObj.role, 'practitioner', 'should have a value of \'practitioner\'')
+      t.equals(participantObj.reference, 'Practitioner/b7aeb450-8bde-11e7-812f-bbfc0872406b', 'should have a value of \'Practitioner/b7aeb450-8bde-11e7-812f-bbfc0872406b\'')
+      t.equals(participantObj.userId, 'b7aeb450-8bde-11e7-812f-bbfc0872406b', 'should have a value of \'b7aeb450-8bde-11e7-812f-bbfc0872406b\'')
+      t.equals(participantObj.altId, 'user@hearth.org', 'should have a value of \'user@hearth.org\'')
+      t.equals(participantObj.requester, true, 'should have a value of \'true\'')
 
-    t.end()
+      done()
+    })
   })
 })
 
 tap.test('Audit Plugin - buildSourceObj()', { autoend: true }, (t) => {
   t.test('should build the sourceObj object', (t) => {
-    // given
-    const ctx = {
-      authenticatedUser: {
-        email: 'jenny@sssa.org',
-        type: 'practitioner',
-        resource: 'Practitioner/b7aeb450-8bde-11e7-812f-bbfc0872406b'
-      },
-      url: '/fhir/Questionnaire?identifier=preoperative-questionnaire',
-      query: { identifier: 'preoperative-questionnaire' },
-      headers: {
-        referer: 'http://localhost:9000/'
+    testServerInit(t, (db, done) => {
+      // given
+      const ctx = {
+        authenticatedUser: {
+          email: 'jenny@sssa.org',
+          type: 'practitioner',
+          resource: 'Practitioner/b7aeb450-8bde-11e7-812f-bbfc0872406b'
+        },
+        url: '/fhir/Questionnaire?identifier=preoperative-questionnaire',
+        query: { identifier: 'preoperative-questionnaire' },
+        headers: {
+          referer: 'http://localhost:9000/'
+        }
       }
-    }
 
-    // when
-    const sourceObj = auditPlugin.buildSourceObj(ctx)
+      // when
+      const sourceObj = auditPlugin.buildSourceObj(ctx)
 
-    t.ok(sourceObj)
+      t.ok(sourceObj)
 
-    t.equals(sourceObj.site, 'Cloud', 'should have a value of \'Cloud\'')
-    t.equals(sourceObj.identifier, 'http://localhost:9000/', 'should have a value of \'http://localhost:9000/\'')
-    t.equals(sourceObj.type[0].system, 'http://hl7.org/fhir/security-source-type', 'should have a value of \'http://hl7.org/fhir/security-source-type\'')
-    t.equals(sourceObj.type[0].code, 3, 'should have a value of \'3\'')
-    t.equals(sourceObj.type[0].display, 'Web Server', 'should have a value of \'Web Server\'')
+      t.equals(sourceObj.site, 'Cloud', 'should have a value of \'Cloud\'')
+      t.equals(sourceObj.identifier, 'http://localhost:9000/', 'should have a value of \'http://localhost:9000/\'')
+      t.equals(sourceObj.type[0].system, 'http://hl7.org/fhir/security-source-type', 'should have a value of \'http://hl7.org/fhir/security-source-type\'')
+      t.equals(sourceObj.type[0].code, 3, 'should have a value of \'3\'')
+      t.equals(sourceObj.type[0].display, 'Web Server', 'should have a value of \'Web Server\'')
 
-    t.end()
+      done()
+    })
   })
 })
 
 tap.test('Audit Plugin - buildObjectObj()', { autoend: true }, (t) => {
   t.test('should build the objectObj object', (t) => {
-    // given
-    const ctx = {
-      authenticatedUser: {
-        email: 'jenny@sssa.org',
-        type: 'practitioner',
-        resource: 'Practitioner/b7aeb450-8bde-11e7-812f-bbfc0872406b'
-      },
-      url: '/fhir/Questionnaire?identifier=preoperative-questionnaire',
-      query: { identifier: 'preoperative-questionnaire' },
-      headers: {
-        referer: 'http://localhost:9000/'
+    testServerInit(t, (db, done) => {
+      // given
+      const ctx = {
+        authenticatedUser: {
+          email: 'jenny@sssa.org',
+          type: 'practitioner',
+          resource: 'Practitioner/b7aeb450-8bde-11e7-812f-bbfc0872406b'
+        },
+        url: '/fhir/Questionnaire?identifier=preoperative-questionnaire',
+        query: { identifier: 'preoperative-questionnaire' },
+        headers: {
+          referer: 'http://localhost:9000/'
+        }
       }
-    }
 
-    // when
-    const objectObj = auditPlugin.buildObjectObj(ctx)
+      // when
+      const objectObj = auditPlugin.buildObjectObj(ctx)
 
-    t.ok(objectObj)
+      t.ok(objectObj)
 
-    t.equals(objectObj.query, ctx.query, 'should have a value of \'{ identifier: \'preoperative-questionnaire\' }\'')
-    t.equals(objectObj.reference.reference, '/fhir/Questionnaire', 'should have a value of \'/fhir/Questionnaire\'')
+      t.equals(objectObj.query, ctx.query, 'should have a value of \'{ identifier: \'preoperative-questionnaire\' }\'')
+      t.equals(objectObj.reference.reference, '/fhir/Questionnaire', 'should have a value of \'/fhir/Questionnaire\'')
 
-    t.end()
+      done()
+    })
   })
 })
 
 tap.test('Audit Plugin - buildAuditEvent()', { autoend: true }, (t) => {
   t.test('should build the AuditEvent object', (t) => {
-    // given
-    const resource = JSON.parse(JSON.stringify(testResourceTemplate))
-    const ctx = {
-      authenticatedUser: {
-        email: 'jenny@sssa.org',
-        type: 'practitioner',
-        resource: 'Practitioner/b7aeb450-8bde-11e7-812f-bbfc0872406b'
-      },
-      url: '/fhir/Questionnaire?identifier=preoperative-questionnaire',
-      query: { identifier: 'preoperative-questionnaire' },
-      headers: {
-        referer: 'http://localhost:9000/'
+    testServerInit(t, (db, done) => {
+      // given
+      const resource = JSON.parse(JSON.stringify(testResourceTemplate))
+      const ctx = {
+        authenticatedUser: {
+          email: 'jenny@sssa.org',
+          type: 'practitioner',
+          resource: 'Practitioner/b7aeb450-8bde-11e7-812f-bbfc0872406b'
+        },
+        url: '/fhir/Questionnaire?identifier=preoperative-questionnaire',
+        query: { identifier: 'preoperative-questionnaire' },
+        headers: {
+          referer: 'http://localhost:9000/'
+        }
       }
-    }
 
-    // when
-    const auditEvent = auditPlugin.buildAuditEvent('read', ctx, 'Patient', resource)
+      // when
+      const auditEvent = auditPlugin.buildAuditEvent('read', ctx, 'Patient', resource)
 
-    t.ok(auditEvent)
-    t.ok(auditEvent.event)
-    t.ok(auditEvent.participant)
-    t.ok(auditEvent.source)
-    t.ok(auditEvent.object)
+      t.ok(auditEvent)
+      t.ok(auditEvent.event)
+      t.ok(auditEvent.participant)
+      t.ok(auditEvent.source)
+      t.ok(auditEvent.object)
 
-    t.end()
+      done()
+    })
   })
 })
 
@@ -262,17 +281,18 @@ tap.test('Audit Plugin - create audit via fhirCore.create()', { autoend: true },
 
         const c = db.collection('AuditEvent')
         c.findOne({ id: auditId }, (err, result) => {
-          console.log(result)
-
           t.error(err)
-          t.ok(result, 'result should exist in the mongo')
+          t.ok(result, 'result should exist in mongo')
 
-          t.equals(result.resourceType, 'AuditEvent')
           t.equals(result.participant.role, 'sysadmin')
+          t.equals(result.participant.altId, 'sysadmin@jembi.org')
+          t.equals(result.object.reference.reference, '/fhir/Patient')
 
           done()
         })
       })
+      
     })
   })
 })
+
