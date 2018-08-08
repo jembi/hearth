4fb0630b8c073393a32aeff6aa63fc6459317545
Ryan Crichton <ryan@jembi.org>
2018-08-08 15:24:56 +0200

    Update service token gen script to support advanced JWT options
    OCRVS-362

--- scripts/generate-api-token.js
+++ scripts/generate-api-token.js
@@ -15,6 +15,9 @@ Usage:
 $ ./scripts/generate-api-token.js --service-name "My Service" --admin-email admin@my-service.org --secret "my secret"
 eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXJ2aWNlIjoiTXkgU2VydmljZSIsImVtYWlsIjoiYWRtaW5AbXktc2VydmljZS5vcmciLCJ0eXBlIjoic2VydmljZSIsImlhdCI6MTUzMjQyNjg2NywiaXNzIjoiSGVhcnRoIn0.bQomDjWkwSrTyYAiX917kiKZvbsh9httwqRGEMvqZak
 
+$ ./scripts/generate-api-token.js -n test -e admin@jembi.org -s "\`cat privKey.pem\`" -u hearth:user -i hearth -a RS256
+eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXJ2aWNlIjoidGVzdCIsImVtYWlsIjoiYWRtaW5AamVtYmkub3JnIiwidHlwZSI6InNlcnZpY2UiLCJpYXQiOjE1MzM3MzQ2NDIsImF1ZCI6ImhlYXJ0aDp1c2VyIiwiaXNzIjoiaGVhcnRoIn0.YmmRJ_MEiqKi3ev0BsfdoAQIikAOlyfw2U9VNHjnuxgZS-8NHp02nN_Dr1yVvGKoOOeqUKdmSTEhRE6w4JSkkC2g91GcD6gG0cMzTXP6jK_t0Kzs0b69fTi4Nfc3XTwHHfbFOzuXrSf-YA-p2XmiZMJNADeNNQsaCUlQfe9adoc
+
 curl -X GET \\
   https://myhearth.org/fhir/Person/222222 \\
   -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXJ2aWNlIjoiTXkgU2VydmljZSIsImVtYWlsIjoiYWRtaW5AbXktc2VydmljZS5vcmciLCJ0eXBlIjoic2VydmljZSIsImlhdCI6MTUzMjQyNjg2NywiaXNzIjoiSGVhcnRoIn0.bQomDjWkwSrTyYAiX917kiKZvbsh9httwqRGEMvqZak"
@@ -23,7 +26,10 @@ Arguments:
 
 -n --service-name\tService name
 -e --admin-email\tService administrator email
--s --secret\t\tSecret or private key`
+-s --secret\t\tSecret or private key. Use -s "\`cat privKey.pem\`" to read from a file.
+-a --algorithm\t\t(Optional) The JWT algorithm you have configured to use in Hearth (default: 'HS256')
+-i --issuer\t\t(Optional) The issuer of the token, set this to the same value you have configured in Hearth (default: 'Hearth')
+-u --audience\t\t(Optional) The audience of the token, if you have configured Hearth to validate Audience then set this the appropriate value`
 
   console.log(
       message
@@ -52,7 +58,10 @@ const processInput = cli(
   [
     [/^(-n|--service-name)$/, 'serviceName'],
     [/^(-e|--admin-email)$/, 'adminEmail'],
-    [/^(-s|--secret)$/, 'secretOrPrivateKey']
+    [/^(-s|--secret)$/, 'secretOrPrivateKey'],
+    [/^(-a|--algorithm)$/, 'algorithm'],
+    [/^(-i|--issuer)$/, 'issuer'],
+    [/^(-u|--audience)$/, 'audience']
   ],
   handleInvalidInput
 )
@@ -60,7 +69,10 @@ const processInput = cli(
 const {
   serviceName,
   adminEmail,
-  secretOrPrivateKey
+  secretOrPrivateKey,
+  issuer,
+  audience,
+  algorithm
 } = processInput(process.argv)
 
 if (!serviceName || !adminEmail || !secretOrPrivateKey) {
@@ -72,7 +84,13 @@ const payload = {
   email: adminEmail,
   type: 'service'
 }
-const options = {issuer: 'Hearth'}
+const options = {
+  algorithm: algorithm || 'HS256',
+  issuer: issuer || 'Hearth'
+}
+if (audience) {
+  options.audience = audience
+}
 
 jwt.sign(payload, secretOrPrivateKey, options, (err, token) => {
   if (err) {
