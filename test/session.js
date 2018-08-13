685fdbfd971a009eb81436e49ca2ef0d09fc2a4f
Ryan Crichton <ryan@jembi.org>
2018-08-13 09:59:11 +0200

    Use paths for JWT keys rather than strings
    OCRVS-362

--- test/session.js
+++ test/session.js
@@ -91,7 +91,7 @@ tap.test('Create session', withServer((t, server) => {
     })
   }))
 
-  t.test('should ereturn 201 status when a session is created with new jwt config - symmetric algorithm', withUser((t, user) => {
+  t.test('should return 201 status when a session is created with new jwt config - symmetric algorithm', withUser((t, user) => {
     configStub.withArgs('authentication:jwt').returns({
       algorithm: 'HS256',
       secret: 'new secret',
@@ -113,30 +113,11 @@ tap.test('Create session', withServer((t, server) => {
     })
   }))
 
-  t.test('should ereturn 201 status when a session is created with new jwt config - asymmetric algorithm', withUser((t, user) => {
+  t.test('should return 201 status when a session is created with new jwt config - asymmetric algorithm', withUser((t, user) => {
     configStub.withArgs('authentication:jwt').returns({
       algorithm: 'RS256',
-      pubKey: `-----BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDdlatRjRjogo3WojgGHFHYLugd
-UWAY9iR3fy4arWNA1KoS8kVw33cJibXr8bvwUAUparCwlvdbH6dvEOfou0/gCFQs
-HUfQrSDv+MuSUMAe8jzKE4qW+jK+xQU9a03GUnKHkkle+Q0pX/g6jXZ7r1/xAK5D
-o2kQ+X5xK9cipRgEKwIDAQAB
------END PUBLIC KEY-----`,
-      privKey: `-----BEGIN RSA PRIVATE KEY-----
-MIICWwIBAAKBgQDdlatRjRjogo3WojgGHFHYLugdUWAY9iR3fy4arWNA1KoS8kVw
-33cJibXr8bvwUAUparCwlvdbH6dvEOfou0/gCFQsHUfQrSDv+MuSUMAe8jzKE4qW
-+jK+xQU9a03GUnKHkkle+Q0pX/g6jXZ7r1/xAK5Do2kQ+X5xK9cipRgEKwIDAQAB
-AoGAD+onAtVye4ic7VR7V50DF9bOnwRwNXrARcDhq9LWNRrRGElESYYTQ6EbatXS
-3MCyjjX2eMhu/aF5YhXBwkppwxg+EOmXeh+MzL7Zh284OuPbkglAaGhV9bb6/5Cp
-uGb1esyPbYW+Ty2PC0GSZfIXkXs76jXAu9TOBvD0ybc2YlkCQQDywg2R/7t3Q2OE
-2+yo382CLJdrlSLVROWKwb4tb2PjhY4XAwV8d1vy0RenxTB+K5Mu57uVSTHtrMK0
-GAtFr833AkEA6avx20OHo61Yela/4k5kQDtjEf1N0LfI+BcWZtxsS3jDM3i1Hp0K
-Su5rsCPb8acJo5RO26gGVrfAsDcIXKC+bQJAZZ2XIpsitLyPpuiMOvBbzPavd4gY
-6Z8KWrfYzJoI/Q9FuBo6rKwl4BFoToD7WIUS+hpkagwWiz+6zLoX1dbOZwJACmH5
-fSSjAkLRi54PKJ8TFUeOP15h9sQzydI8zJU+upvDEKZsZc/UhT/SySDOxQ4G/523
-Y0sz/OZtSWcol/UMgQJALesy++GdvoIDLfJX5GBQpuFgFenRiRDabxrE9MNUZ2aP
-FaFp+DyAe+b4nDwuJaW2LURbr8AEZga7oQj0uYxcYw==
------END RSA PRIVATE KEY-----`,
+      pubKey: `test/resources/jwt-certs/pubKey.pem`,
+      privKey: `test/resources/jwt-certs/privKey.pem`,
       issuer: 'hearth',
       setAudience: 'hearth:example-app1',
       validateAudience: '^hearth:example-app\\d+$'
