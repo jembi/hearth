bca8570434d87033a09d74a46cdcfbb86da32b96
brad <bradford.sawadye@jembi.org>
2019-07-29 16:54:07 +0200

    fix lint errors
    IHS-129

--- perfomance/volume.js
+++ perfomance/volume.js
@@ -1,39 +1,39 @@
 import http from 'k6/http'
 import {check} from 'k6'
 
+/* global __ENV */
 const BASE_URL = __ENV.BASE_URL || 'http://localhost:3447'
 
 // PATH - the path to the resource. Default is path to the patient resource
 const RESOURCE_PATH = __ENV.RESOURCE_PATH || '/fhir/patient'
 
 export const options = {
-	vus: 10,
-	iterations: 1000,
-	thresholds: {
-		http_req_receiving: ['p(95)<100'],
-		http_req_duration: ['p(95)<100']
-	},
-	noVUConnectionReuse: true,
-	discardResponseBodies: true
+  vus: 10,
+  iterations: 1000,
+  thresholds: {
+    http_req_receiving: ['p(95)<100'],
+    http_req_duration: ['p(95)<100']
+  },
+  noVUConnectionReuse: true,
+  discardResponseBodies: true
 }
 
 const makeGetRequest = () => {
-	const response = http.get(
-		`${BASE_URL}${RESOURCE_PATH}`,
-		{
-			headers: {
-				Accept: 'application/json'
-			},
-			tags: {
-				name: 'Get request'
-			}
-		}
-	)
-	check(response, {
-		'status code is 200': r => r.status === 200
-	})
-} 
+  const response = http.get(`${BASE_URL}${RESOURCE_PATH}`,
+    {
+      headers: {
+        Accept: 'application/json'
+      },
+      tags: {
+        name: 'Get request'
+      }
+    })
 
-export default function() {
-	makeGetRequest()
+  check(response, {
+    'status code is 200': r => r.status === 200
+  })
+}
+
+export default function () {
+  makeGetRequest()
 }
