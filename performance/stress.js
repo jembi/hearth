9f22571ebf44286aca7e9c5310664210229dc975
brad <bradford.sawadye@jembi.org>
2019-08-12 09:58:10 +0200

    lint error fix
    IHS-137

--- performance/stress.js
+++ performance/stress.js
@@ -8,28 +8,28 @@ const BASE_URL = __ENV.BASE_URL || 'http://localhost:3447'
 const RESOURCE_PATH = __ENV.RESOURCE_PATH || '/fhir/Patient'
 
 export const options = {
-    vus: 100,
-    duration: '2m',
-    thresholds: {
-        http_req_duration: ['p(95)<60']
-    },
-    noVUConnectionReuse: true,
-    discardResponseBodies: true
+  vus: 100,
+  duration: '2m',
+  thresholds: {
+    http_req_duration: ['p(95)<60']
+  },
+  noVUConnectionReuse: true,
+  discardResponseBodies: true
 }
 
 const makeGetRequest = () => {
-    const response = http.get(`${BASE_URL}${RESOURCE_PATH}`, { headers: {
-        Accept: 'application/json'
-    },
-    tags: {
-        name: 'Get request'
-    }
-    })
-    check(response, {
-        'status code is 200': r => r.status === 200
-    })
+  const response = http.get(`${BASE_URL}${RESOURCE_PATH}`, { headers: {
+    Accept: 'application/json'
+  },
+  tags: {
+    name: 'Get request'
+  }
+  })
+  check(response, {
+    'status code is 200': r => r.status === 200
+  })
 }
 
 export default function () {
-    makeGetRequest()
+  makeGetRequest()
 }
