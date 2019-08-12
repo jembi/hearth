95c7967976633bd792a554b9450df04eaebc5a05
brad <bradford.sawadye@jembi.org>
2019-08-12 17:06:41 +0200

    add a bundle request test suite
    This adds a hearth performance test in which multiple resources sre created in a bundle resource.
    
    IHS-138

---
+++ performance/patient-encounter-observation-bundle-create.js
@@ -0,0 +1,98 @@
+import http from 'k6/http'
+import {check} from 'k6'
+import {patientResource} from './resources/patient.js'
+import {encounterResource} from './resources/encounter.js'
+import { heightObservation, weightObservation } from './resources/observation.js'
+
+/* global __ENV */
+const BASE_URL = __ENV.BASE_URL || 'http://localhost:3447'
+
+export const options = {
+  vus: 10,
+  iterations: 1000,
+  thresholds: {
+    http_req_receiving: ['p(95)<100'],
+    http_req_duration: ['p(95)<100']
+  },
+  noVUConnectionReuse: true,
+  discardResponseBodies: true
+}
+
+let patientResourceFullUrl
+let encounterResourceFullUrl
+
+const bundleResource = {
+  "resourceType": "Bundle",
+  "type": 'transaction',
+  "timestamp": Date.now(),
+  "entry": [
+    {
+      "fullUrl": patientResourceFullUrl,
+      "resource": patientResource,
+      "request": {
+        "method": "POST",
+        "url": `Patient`, 
+      }
+    },
+    {
+      "fullUrl": encounterResourceFullUrl,
+      "resource": encounterResource,
+      "request": {
+        "method": "POST",
+        "url": `Encounter`, 
+      }
+    },
+    {
+      "resource": heightObservation,
+      "request": {
+        "method": "POST",
+        "url": `Observation`, 
+      }
+    },
+    {
+      "resource": weightObservation,
+      "request": {
+        "method": "POST",
+        "url": `Observation`, 
+      }
+    }, 
+  ]
+}
+
+const makeBundleRequest = () => {
+  // Create full urls for the resources so they can reference each other. THe math random method is used to ensure the urls are unique.
+  // The uuid npm library could have been used but K6 does not run in node, and the modules would have to be bundled. 
+  patientResourceFullUrl = `urn:uuid:${Date.now() + Math.floor(Math.random() * 1000)}`
+  encounterResourceFullUrl = `urn:uuid:${Date.now() + Math.floor(Math.random() * 1000)}`
+
+  bundleResource.entry[0].fullUrl = patientResourceFullUrl
+  bundleResource.entry[1].fullUrl = encounterResourceFullUrl
+
+  encounterResource.patient.reference = patientResourceFullUrl
+  heightObservation.context.reference = patientResourceFullUrl
+  weightObservation.context.reference = patientResourceFullUrl
+  heightObservation.subject.reference = encounterResourceFullUrl
+  weightObservation.subject.reference = encounterResourceFullUrl
+
+  const response = http.post(
+    `${BASE_URL}/fhir`,
+    JSON.stringify(bundleResource),
+    {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json+fhir'
+      },
+      tags: {
+        name: 'Create Bundle Resource'
+      }
+    }
+  )
+  
+  check(response, {
+    'status code for bundle creation is 200': r => r.status === 200
+  })
+}
+
+export default function () {
+  makeBundleRequest()
+}
